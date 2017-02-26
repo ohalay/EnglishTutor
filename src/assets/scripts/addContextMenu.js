8d096ad498938c0baed329bed9ba2ed68bd86060
ohalay <olehhalay@ukr.net>
2017-02-26 11:33:03 +0200

    -add fireBase to background script

--- src/assets/scripts/addContextMenu.js
+++ src/assets/scripts/addContextMenu.js
@@ -1,9 +1,22 @@
+
+var config = {
+    apiKey: "AIzaSyCVuhcVbj1EfrB3iP6I9LQGce8Wzlk-k8U",
+    authDomain: "eanglish-tutor.firebaseapp.com",
+    databaseURL: "https://eanglish-tutor.firebaseio.com",
+    storageBucket: "eanglish-tutor.appspot.com",
+    messagingSenderId: "456223131583"
+};
+firebase.initializeApp(config);
+
 function translateOnClick(e){
+    var ref = firebase.database().ref('vocabilary');
+    ref.child(e.selectionText).set(e.selectionText);
+
     chrome.storage.sync.set({'lastWord' : e.selectionText})
 }
 
 chrome.contextMenus.create({
-    'title': 'Translate',
+    'title': 'Add to vocabilary',
     'contexts': ['selection'],
     'onclick' : translateOnClick
 });
